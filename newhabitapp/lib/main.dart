fdb01d8616d9ec7baa611231112b56dcd6065389
aastha <aastha311@gmail.com>
2020-09-06 17:23:52 +0100

    Sign in Pages

--- newhabitapp/lib/main.dart
+++ newhabitapp/lib/main.dart
@@ -1,24 +1,224 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-    title: 'Welcome to Flutter',
-    home: Scaffold(
-    appBar: AppBar(
-    title: Text('Welcome to Flutter'),
-    ),
-    body: Center(
-    child: Text(wordPair.asCamelCase),
-        ),
+    return new MaterialApp(home: new HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Color primaryColor = Color(0xff18203d);
+    final Color logoGreen = Color(0xff25bcbb);
+
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          //We take the image from the assets
+          Image.asset(
+            'assets/images/2.jpg',
+            height: 250,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          //Texts and Styling of them
+          Text(
+            'Welcome to Habit App !',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'A one-stop portal for you to learn the latest technologies from SCRATCH',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          //Our MaterialButton which when pressed will take us to a new screen named as
+          //LoginScreen
+          MaterialButton(
+            elevation: 0,
+            height: 50,
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => LoginScreen()));
+            },
+            color: logoGreen,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Get Started',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                Icon(Icons.arrow_forward_ios)
+              ],
+            ),
+            textColor: Colors.white,
+          )
+        ],
       ),
     );
   }
 }
 
+class LoginScreen extends StatelessWidget {
+  final Color primaryColor = Color(0xff18203d);
+  final Color secondaryColor = Color(0xff232c51);
 
+  final Color logoGreen = Color(0xff25bcbb);
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: primaryColor,
+        body: Container(
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sign in to TGD and continue',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 28),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Enter your email and password below to continue to the The Growing Developer and let the learning begin!',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 14),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                _buildTextField(
+                    nameController, Icons.account_circle, 'Username'),
+                SizedBox(height: 20),
+                _buildTextField(passwordController, Icons.lock, 'Password'),
+                SizedBox(height: 30),
+                MaterialButton(
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () {},
+                  color: logoGreen,
+                  child: Text('Login',
+                      style: TextStyle(color: Colors.white, fontSize: 16)),
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 20),
+                MaterialButton(
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () {
+                    _signInWithGoogle();
+                    //Here goes the logic for Google SignIn discussed in the next section
+                  },
+                  color: Colors.blue,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.google),
+                      SizedBox(width: 10),
+                      Text('Sign-in using Google',
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    ],
+                  ),
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 100),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: _buildFooterLogo(),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  _buildFooterLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/2.jpg',
+          height: 40,
+        ),
+        Text('The Growing Developer',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.openSans(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+    _signInWithGoogle() async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+    await googleUser.authentication;
+
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+
+    final FirebaseUser user =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+  }
+
+  _buildTextField(
+      TextEditingController controller, IconData icon, String labelText) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+          color: secondaryColor, border: Border.all(color: Colors.blue)),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10),
+            labelText: labelText,
+            labelStyle: TextStyle(color: Colors.white),
+            icon: Icon(
+              icon,
+              color: Colors.white,
+            ),
+            // prefix: Icon(icon),
+            border: InputBorder.none),
+      ),
+    );
+  }
+}
