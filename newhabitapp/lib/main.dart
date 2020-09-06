67016e85f5440aefdba46e00c5eb3ef6dc5df934
aastha <aastha311@gmail.com>
2020-09-06 17:32:15 +0100

    Remove other projects

--- newhabitapp/lib/main.dart
+++ newhabitapp/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
